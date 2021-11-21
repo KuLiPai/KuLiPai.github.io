3921198d86c5e831226d1f12e32c51cdf03a2d49
苦小怕 <94775075+badmll@users.noreply.github.com>
2021-11-21 14:36:19 +0800

    Add files via upload

--- lua/阶乘/main.lua
+++ lua/阶乘/main.lua
@@ -1,1 +1,30 @@
+require "import"
+import "android.app.*"
+import "android.os.*"
+import "android.widget.*"
+import "android.view.*"
+import "layout"
 
+activity.setTheme(R.Theme_Blue)
+activity.setTitle("！")
+activity.setContentView(loadlayout(layout))
+
+x=6
+l=x
+n=1
+while true
+do
+if x==1 then
+print(e)
+break
+elseif x==l then
+w=x
+x=x-1
+e=n*w*x
+n=e
+else
+x=x-1
+e=e*x
+
+end
+end
