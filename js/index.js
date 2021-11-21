2415ccb34c08633301d6f399fe88e802c053df35
苦小怕 <94775075+badmll@users.noreply.github.com>
2021-11-21 15:08:22 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('Shape|Shifter|Type|to start|#rectangle|#countdown 3||');
+      S.UI.simulate('欢迎|来到|io|❤️');
     }
 
     S.Drawing.loop(function () {
